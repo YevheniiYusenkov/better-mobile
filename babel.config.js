8afacd0e4e00281244dbac46200b342b90dd16bb
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-08-02 13:32:59 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./'],
+        alias: {
+          '@better/screens': './src/screens/index.ts',
+          '@better/components': './src/components/index.ts',
+        },
+      },
+    ],
+  ],
 };
